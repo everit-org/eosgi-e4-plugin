1760c3f09013b612b7ef65cf896761eada56daaf
Zsolt Doma <zsolt.doma@gmail.com>
2015-08-11 16:09:19 +0200

    StartHandler works

--- plugin/src/org/everit/e4/eosgi/plugin/ui/command/StartHandler.java
+++ plugin/src/org/everit/e4/eosgi/plugin/ui/command/StartHandler.java
@@ -1,45 +1,47 @@
 package org.everit.e4.eosgi.plugin.ui.command;
 
+import java.util.logging.Logger;
+
+//import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
 
+/**
+ * Handler implementation for start the current selected dist project.
+ */
 public class StartHandler implements IHandler {
 
+  private static final Logger LOGGER = Logger.getLogger(StartHandler.class.getName());
+
   @Override
-  public void addHandlerListener(IHandlerListener arg0) {
-    System.out.println("add handler listener");
+  public void addHandlerListener(final IHandlerListener arg0) {
   }
 
   @Override
   public void dispose() {
-    // TODO Auto-generated method stub
-
+    LOGGER.info("dispose");
   }
 
   @Override
   public Object execute(final ExecutionEvent arg0) throws ExecutionException {
-    System.out.println("execute command");
+    LOGGER.info("execute");
     return null;
   }
 
   @Override
   public boolean isEnabled() {
-    // TODO Auto-generated method stub
-    return false;
+    return true;
   }
 
   @Override
   public boolean isHandled() {
-    // TODO Auto-generated method stub
-    return false;
+    return true;
   }
 
   @Override
-  public void removeHandlerListener(IHandlerListener arg0) {
-    // TODO Auto-generated method stub
-
+  public void removeHandlerListener(final IHandlerListener arg0) {
   }
 
 }
